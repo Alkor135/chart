2faae873b0ee7f744a2769f441b692d376a3ed87
Aleksey Manin <Alkor135@gmail.com>
2021-02-08 20:21:13 +0300

    delta

--- RTS_Delta_DeltaTime.py
+++ RTS_Delta_DeltaTime.py
@@ -14,13 +14,15 @@ symbol = 'RTS'
 2019-04-08 10:00:42,121210.0,121240.0,121100.0,121100.0,23,546
 2019-04-08 10:01:05,121100.0,121180.0,121030.0,121180.0,79,1071
 """
-df = pd.read_csv('c:/data_finam_quote_csv/500_delta.csv', names=["date_time",
-                                                                 "open",
-                                                                 "high",
-                                                                 "low",
-                                                                 "close",
-                                                                 "delta_time",
-                                                                 "ticks"], delimiter=',')
+df = pd.read_csv('c:/data_finam_quote_csv/500_delta.csv',
+                 names=["date_time",
+                        "open",
+                        "high",
+                        "low",
+                        "close",
+                        "delta_time",
+                        "ticks"],
+                 delimiter=',')
 
 # Меняем индекс и делаем его типом datetime
 df = df.set_index(pd.to_datetime(df['date_time'], format='%Y-%m-%d %H:%M:%S'))
