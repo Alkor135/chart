b354cf6be0abc94218db5f7cf128ffec8327cab0
Aleksey Manin <Alkor135@gmail.com>
2021-03-08 06:08:41 +0300

    мелкие правки

--- finplot_history_data/file_tick_to_delta_chart.py
+++ finplot_history_data/file_tick_to_delta_chart.py
@@ -8,6 +8,9 @@ class DeltaBarCreate:
     def __init__(self, file_patch, delta_period):
         self.df = pd.read_csv(file_patch)
         self.df_tick = pd.DataFrame(columns='date_time last vol'.split(' '))
+        self.delta_period = delta_period
+
+
 
 
 if __name__ == '__main__':
